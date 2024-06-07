5f496d96e5decd50aac88f54651ff2661d1f9fdf
Thirukumaran <113665973+19106128@users.noreply.github.com>
2024-06-07 12:05:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+function one()
+{
+    var img=document.getElementById('img1')
+          img.src='./PNG/roll1.jpg';
+}
+
+function two()
+{
+    var img=document.getElementById('img1')
+          img.src='./PNG/image.png';
+}
+
+function three()
+{
+    var img=document.getElementById('img1')
+          img.src='./PNG/roll2.jpg';
+}
+
+
+function golink(link){
+   window.open('https://www.fylehq.com/')
+}
+
+
+
